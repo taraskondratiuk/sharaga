024bc2532be45b6e8d8bcc2c1b2c51727622296c
Taras <kondratiuk.taras123@gmail.com>
2019-12-22 19:14:15 +0200

    readers writers added

--- 3kurs/1sem/os/1/readers_writers_win32.cpp
+++ 3kurs/1sem/os/1/readers_writers_win32.cpp
@@ -1,67 +1,157 @@
-#include "iostream"
-#include "windows.h"
+#include <windows.h>
+#include <cstdio>
 
-#define PHIL_NUM 5
+#define NUM_READERS 5
 
-using namespace std;
+#define NUM_WRITERS 2
 
-HANDLE events[PHIL_NUM];
+CRITICAL_SECTION single_writer, output, writers_queue, library, readers_queue;
 
-HANDLE output_mutex;
+int file, writer_counter = 0, reader_counter = 0;
 
-DWORD WINAPI eat(void *);
+DWORD WINAPI functionRead(LPVOID arg);
+
+DWORD WINAPI functionWrite(LPVOID arg);
+
 
 int main() {
+    DWORD threadId;
 
-    DWORD dwThreadId;
-    output_mutex = CreateMutex(nullptr, false, NULL);
+    HANDLE readersThreads[NUM_READERS];
+    HANDLE writersThreads[NUM_WRITERS];
 
-    for (auto &event : events) {
-        event = CreateEvent(nullptr, false, true, nullptr);
+    int readerIds[NUM_READERS];
+    int writerIds[NUM_WRITERS];
+
+    InitializeCriticalSection(&single_writer);
+    InitializeCriticalSection(&output);
+    InitializeCriticalSection(&writers_queue);
+    InitializeCriticalSection(&readers_queue);
+    InitializeCriticalSection(&library);
+
+    for (int j = 0; j < NUM_WRITERS; ++j) {
+        writerIds[j] = j;
+        writersThreads[j] = CreateThread(nullptr, 0, &functionWrite, &writerIds[j], 0, &threadId);
     }
 
-    int phils[PHIL_NUM];
-    HANDLE threads[PHIL_NUM];
-
-    for (int i = 0; i < PHIL_NUM; ++i) {
-        phils[i] = i + 1;
-        threads[i] = CreateThread(NULL, 0, eat, &phils[i], 0, &dwThreadId);
+    Sleep(1000);
+    for (int i = 0; i < NUM_READERS; ++i) {
+        readerIds[i] = i;
+        readersThreads[i] = CreateThread(nullptr, 0, &functionRead, &readerIds[i], 0, &threadId);
     }
 
-    for (auto &thread : threads) {
-        WaitForSingleObject(thread, INFINITE);
-    }
+
+    WaitForMultipleObjects(NUM_WRITERS, writersThreads, true, INFINITE);
+    WaitForMultipleObjects(NUM_WRITERS, readersThreads, true, INFINITE);
+
+    DeleteCriticalSection(&single_writer);
+    return 0;
+
 }
 
+DWORD WINAPI functionRead(LPVOID arg) {
+    int id = *static_cast<int *>(arg) + 1;
+    for (int i = 0; i < 100; ++i) {
+        EnterCriticalSection(&readers_queue);
 
-DWORD WINAPI eat(void *arg) {
-    int *philNum = (int *) arg;
 
-    int lForkIndex = *philNum - 1;
-    int rForkIndex = *philNum;
-    if (rForkIndex == PHIL_NUM) {
-        rForkIndex = 0;
-    }
+        EnterCriticalSection(&output);
+        printf("reader %d came to queue\n", id);
+        LeaveCriticalSection(&output);
 
-    for (int j = 0; j < 10; j++) {
-
-        if (WaitForSingleObject(events[lForkIndex], 0) == WAIT_OBJECT_0) {
-            if (WaitForSingleObject(events[rForkIndex], 0) == WAIT_OBJECT_0) {
-
-                WaitForSingleObject(output_mutex, INFINITE);
-                printf("philosopher %d eating\n", *philNum);
-                ReleaseMutex(output_mutex);
-
-                Sleep(rand() % 2000);
-
-                WaitForSingleObject(output_mutex, INFINITE);
-                printf("philosopher %d thinking\n", *philNum);
-                ReleaseMutex(output_mutex);
-
-                SetEvent(events[rForkIndex]);
-            }
-            SetEvent(events[lForkIndex]);
+        reader_counter++;
+        if (reader_counter == 1) {
+            EnterCriticalSection(&library);
+            EnterCriticalSection(&output);
+            printf("reader %d locked lib\n", id);
+            LeaveCriticalSection(&output);
         }
-        Sleep(rand() % 2000);
+
+        LeaveCriticalSection(&readers_queue);
+
+        EnterCriticalSection(&output);
+        printf("reader %d reading : %d\n", id, file);
+        LeaveCriticalSection(&output);
+
+        Sleep(1000 + rand() % 1000);
+
+
+        EnterCriticalSection(&output);
+        printf("reader %d left\n", id);
+        LeaveCriticalSection(&output);
+
+        EnterCriticalSection(&readers_queue);
+
+        reader_counter--;
+        if (reader_counter == 0) {
+            EnterCriticalSection(&output);
+            printf("reader %d unlocked lib\n", id);
+            LeaveCriticalSection(&output);
+            LeaveCriticalSection(&library);
+        }
+        LeaveCriticalSection(&readers_queue);
+
+        Sleep(5000 + rand() % 5000);
     }
+
+
+    return 0;
+}
+
+DWORD WINAPI functionWrite(LPVOID arg) {
+    int id = *static_cast<int *>(arg) + 1;
+    for (int i = 0; i < 10; ++i) {
+
+
+        EnterCriticalSection(&writers_queue);
+        writer_counter++;
+
+        EnterCriticalSection(&output);
+        printf("writer %d came to queue\n", id);
+        LeaveCriticalSection(&output);
+
+
+        if (writer_counter == 1) {
+            EnterCriticalSection(&library);
+            EnterCriticalSection(&output);
+            printf("writer %d locked lib\n", id);
+            LeaveCriticalSection(&output);
+        }
+
+
+        LeaveCriticalSection(&writers_queue);
+
+
+        EnterCriticalSection(&single_writer);
+
+        EnterCriticalSection(&output);
+        printf("writer %d writing\n", id);
+        LeaveCriticalSection(&output);
+
+        Sleep(1000);
+        file = rand() % 1000;
+
+        EnterCriticalSection(&output);
+        printf("new file : %d\n", file);
+
+        printf("writer %d left\n", id);
+        LeaveCriticalSection(&output);
+
+        LeaveCriticalSection(&single_writer);
+
+        EnterCriticalSection(&writers_queue);
+        writer_counter--;
+        if (writer_counter == 0) {
+            EnterCriticalSection(&output);
+            printf("writer %d unlocked lib\n", id);
+            LeaveCriticalSection(&output);
+            LeaveCriticalSection(&library);
+        }
+
+
+        LeaveCriticalSection(&writers_queue);
+
+        Sleep(4000 + rand() % 2000);
+    }
+    return 0;
 }
